4ffa0d9dfc75f4ac1ca7ca85a4caf5c91a2edd25
saulocapistrano <saulocapistrano@gmail.com>
2025-02-20 15:40:10 -0300

    test: add test for application startup with Testcontainers

--- src/test/java/com/finguard/apifinguardpayments/TestApiFinguardPaymentsApplication.java
+++ src/test/java/com/finguard/apifinguardpayments/TestApiFinguardPaymentsApplication.java
@@ -1,11 +1,23 @@
 package com.finguard.apifinguardpayments;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
-public class TestApiFinguardPaymentsApplication {
+import static org.assertj.core.api.Assertions.assertThatCode;
 
-	public static void main(String[] args) {
-		SpringApplication.from(ApiFinguardPaymentsApplication::main).with(TestcontainersConfiguration.class).run(args);
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class TestApiFinguardPaymentsApplicationTest {
+
+	@Test
+	void applicationStartsSuccessfully() {
+		String[] args = {};
+		assertThatCode(() ->
+				SpringApplication.from(ApiFinguardPaymentsApplication::main)
+						.with(TestcontainersConfiguration.class)
+						.run(args)
+		).doesNotThrowAnyException();
 	}
-
 }

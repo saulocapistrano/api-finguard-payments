afeaee8572c741aa884c18ce5c7f85ecdf16adab
saulocapistrano <saulocapistrano@gmail.com>
2025-02-22 22:25:57 -0300

    refactor: improve request handling in PaymentController.java

--- src/main/java/com/finguard/apifinguardpayments/web/PaymentController.java
+++ src/main/java/com/finguard/apifinguardpayments/web/PaymentController.java
@@ -11,6 +11,7 @@ import java.math.BigDecimal;
 import java.util.List;
 
 @RestController
+@RequestMapping("/payments")
 public class PaymentController implements PaymentApi {
 
     private final PaymentService paymentService;

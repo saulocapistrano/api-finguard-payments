1c53505c42ec17bdbf6e37f1c5217d7760296ff7
saulocapistrano <saulocapistrano@gmail.com>
2025-02-22 21:30:31 -0300

    test: configure Testcontainers for PostgreSQL and Redis integration tests

--- src/test/java/com/finguard/apifinguardpayments/TestcontainersConfiguration.java
+++ src/test/java/com/finguard/apifinguardpayments/TestcontainersConfiguration.java
@@ -4,8 +4,8 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.kafka.KafkaContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
@@ -14,19 +14,34 @@ class TestcontainersConfiguration {
 	@Bean
 	@ServiceConnection
 	KafkaContainer kafkaContainer() {
-		return new KafkaContainer(DockerImageName.parse("apache/kafka-native:latest"));
+		KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:latest"));
+		kafka.start();
+		System.setProperty("KAFKA_BOOTSTRAP_SERVERS", kafka.getBootstrapServers());
+		return kafka;
 	}
 
 	@Bean
 	@ServiceConnection
 	PostgreSQLContainer<?> postgresContainer() {
-		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+		PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(DockerImageName.parse("postgres:15"))
+				.withDatabaseName("payments_db")
+				.withUsername("saulo")
+				.withPassword("123456");
+
+		postgres.start();
+		System.setProperty("spring.datasource.url", postgres.getJdbcUrl());
+		System.setProperty("spring.datasource.username", postgres.getUsername());
+		System.setProperty("spring.datasource.password", postgres.getPassword());
+
+		return postgres;
 	}
 
 	@Bean
 	@ServiceConnection(name = "redis")
 	GenericContainer<?> redisContainer() {
-		return new GenericContainer<>(DockerImageName.parse("redis:latest")).withExposedPorts(6379);
+		GenericContainer<?> redis = new GenericContainer<>(DockerImageName.parse("redis:latest"))
+				.withExposedPorts(6379);
+		redis.start();
+		return redis;
 	}
-
 }
